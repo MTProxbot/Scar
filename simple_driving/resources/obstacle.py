7a07a143dd4fdc0cb9eef0025e1417cb6a45bd0e
Matthew T <145758227+MTProxbot@users.noreply.github.com>
2025-04-29 00:10:24 +1000

    Update obstacle.py

--- simple_driving/resources/obstacle.py
+++ simple_driving/resources/obstacle.py
@@ -5,5 +5,8 @@ class Obstacle:
 
     def __init__(self, client, base):
         f_name = os.path.join(os.path.dirname(__file__), 'cube.urdf')
-         self.obstacle_id = client.loadURDF(fileName=f_name,
+         self.obstacle = client.loadURDF(fileName=f_name,
                                             basePosition=[base[0], base[1], 0])
+
+    def get_ids(self):
+        return self.obstacle
