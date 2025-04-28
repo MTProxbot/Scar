93cbdd85311c9112f62b49382ef9cdab0b1a4f25
Matthew T <145758227+MTProxbot@users.noreply.github.com>
2025-04-28 21:22:37 +1000

    Create Obstacle.py

---
+++ simple_driving/resources/Obstacle.py
@@ -0,0 +1,10 @@
+import pybullet as p
+import os
+
+class Obstacle:
+
+    def __init__(self, client, base):
+        f_name = os.path.join(os.path.dirname(__file__), 'cube.urdf')
+            self.obstacle_id = client.loadURDF(fileName=f_name,
+                                               basePosition=base_pos,
+                                               basePosition=[0, 0, 0.1])
